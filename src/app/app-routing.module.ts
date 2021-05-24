0ff71e30e5872d761f80f6180d0367493fe6cea2
gone <nattlao@gmail.com>
2021-05-24 14:16:52 +0700

    no message

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {LoginComponent} from 'src/app/login/login.component'
+import {UserComponent} from 'src/app/user/user.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'user', component: UserComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
